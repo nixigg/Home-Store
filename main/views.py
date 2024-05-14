2d2ad414e6bb803d694ad7a2a4372212ad486867
Sergiu <nixigg@gmail.com>
2024-05-13 23:41:10 -0700

    The main app is ready, pages index and about.

--- main/views.py
+++ main/views.py
@@ -4,16 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page of home store',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': True,
+        'title': 'Home - Main',
+        'content': "Furniture Store HOME",
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About Page')
+    context = {
+        'title': "Home - About us",
+        'content': "About Us",
+        'text_on_page': "Our store is one of the best in the country."
+    }
+    return render(request, 'main/about.html', context)
 
